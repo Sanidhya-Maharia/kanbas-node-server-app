23fb60acc2191d1bd8551f85b3bfd8ae46d70de6
Sanidhya Maharia <sanidhyamaharia@gmail.com>
2024-11-12 19:37:06 -0500

    Lab5 additions

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from 'express';
-import Hello from "./Hello.js"
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
 const app = express();
-Hello(app)
-app.listen(4000);
+app.use(express.json());
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
