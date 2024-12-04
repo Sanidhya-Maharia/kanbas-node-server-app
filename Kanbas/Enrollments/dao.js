4d0f8688d2410ec8bb4cd48c50884d0ba2a86ad4
Sanidhya Maharia <sanidhyamaharia@gmail.com>
2024-12-04 18:59:06 -0500

    hopefully this does not break the deployment (assignments part left)

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,17 +1,19 @@
 import Database from "../Database/index.js";
-export function enrollUserInCourse(userId, courseId) {
-    const { enrollments } = Database;
-    enrollments.push({ _id: Date.now(), user: userId, course: courseId });
-}
+import model from "./model.js";
 export function getAllEnrollments() {
-    return Database.enrollments;
+    return model.find();
 }
-export function createEnrollment(enrollment) {
-    const newEnrollment = {...enrollment, _id: Date.now().toString()};
-    Database.enrollments = [...Database.enrollments, newEnrollment];
-    return newEnrollment;
+export async function findCoursesForUser(userId) {
+    const enrollments = await model.find({ user: userId }).populate("course");
+    return enrollments.map((enrollment) => enrollment.course);
 }
-export function removeEnrollment(enrollmentId) {
-    const {enrollments} = Database;
-    Database.enrollments = enrollments.filter((e) => e._id !== enrollmentId);
+export async function findUsersForCourse(courseId) {
+    const enrollments = await model.find({ course: courseId }).populate("user");
+    return enrollments.map((enrollment) => enrollment.user);
+}
+export function enrollUserInCourse(user, course) {
+    return model.create({ user, course });
+}
+export function unenrollUserFromCourse(user, course) {
+    return model.deleteOne({ user, course });
 }
